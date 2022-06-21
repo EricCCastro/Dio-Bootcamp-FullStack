b60bf006a86dfa803511170acc8be90947aedd18
Eric Castro <eric-henrique44@hotmail.com>
2022-06-20 23:33:42 -0300

    funcoes.mjs

---
+++ Spread Fullstack Developer/Modulo Javascript/Modulos/funcoes.mjs
@@ -0,0 +1,14 @@
+function mostraIdade(nome, idade){
+    return `A idade de ${nome} e ${idade}`;
+}
+
+function mostraCidade(nome, cidade){
+    return `A cidade de ${nome} e ${cidade}`;
+}
+
+function mostraHobby(nome, hobby){
+    return `O Hobby de ${nome} e ${hobby}`;
+}
+
+export { mostraIdade, mostraCidade, mostraHobby }
+;
